1b032104b87ff1ab06abaf90c0a2a151cff3d033
Robert Hypax Ndiramiye <ndiramiye.robert@gmail.com>
2023-01-24 15:14:50 +0200

    made my name is bond

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,11 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.WriteLine();
+        Console.Write("What is you first name? ");
+        string fname = Console.ReadLine();
+        Console.Write("What is you last name? ");
+        string lname = Console.ReadLine();
+        Console.WriteLine("Your name is " + lname + ", " + fname + " " + lname);
     }
 }
