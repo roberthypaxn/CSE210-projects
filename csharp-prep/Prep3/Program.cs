60dcbbc4a91d65dbb47d539b92bd5f16eb1b3392
Robert Hypax Ndiramiye <ndiramiye.robert@gmail.com>
2023-01-24 18:09:28 +0200

    asked for user input

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,11 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Console.WriteLine("What is the magic number? ");
+        string magicNum = Console.ReadLine();
+        int magicNumber = int.Parse(magicNum);
+        Console.WriteLine("what is your guess? ");
+        string guessText = Console.ReadLine();
+        int guess = int.Parse(guessText);
     }
 }
