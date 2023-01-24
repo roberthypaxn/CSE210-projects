248286ab4f6d16c7d2960967ce90fd5e0cbce0dc
Robert Hypax Ndiramiye <ndiramiye.robert@gmail.com>
2023-01-24 21:19:56 +0200

    created game: guess magic number

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,11 +4,43 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("What is the magic number? ");
+        Console.Write("What is the magic number? ");
         string magicNum = Console.ReadLine();
         int magicNumber = int.Parse(magicNum);
-        Console.WriteLine("what is your guess? ");
+        
+        Console.Write("What is your guess? ");
         string guessText = Console.ReadLine();
         int guess = int.Parse(guessText);
+
+        //if (guess < magicNumber)
+        //{
+        //    Console.WriteLine("Higher");
+        //}
+        //else if (guess > magicNumber)
+        //{
+        //    Console.WriteLine("Lower");
+        //}
+        
+        while (guess != magicNumber)
+        {
+            if (guess < magicNumber)
+            {
+                Console.WriteLine("Higher ");
+                string guessedNum = Console.ReadLine();
+                guess = int.Parse(guessedNum);
+
+            }
+            else if (guess > magicNumber)
+            {
+                Console.WriteLine("Lower");
+                string guessedNum = Console.ReadLine();
+                guess = int.Parse(guessedNum);
+            }
+        }
+
+        if (guess == magicNumber)
+        {
+            Console.WriteLine("You guessed it!");
+        }
     }
 }
