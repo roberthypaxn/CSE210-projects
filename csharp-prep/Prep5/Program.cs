17288717080308df83ae32cbc02d9c14d83040e5
Robert Hypax Ndiramiye <ndiramiye.robert@gmail.com>
2023-01-25 19:26:19 +0200

    Created DisplayWelcome function

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        //Function that displays the message, "Welcome to the Program!"
+        static void DisplayWelcome()
+        {
+            Console.WriteLine("Welcome to the Program!");
+        }
+        DisplayWelcome();
+
     }
 }
