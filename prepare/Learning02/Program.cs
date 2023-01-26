403fe89df3f2030d3d74ce8d013c6d40804b0f3a
Robert Hypax Ndiramiye <ndiramiye.robert@gmail.com>
2023-01-26 14:28:59 +0200

    Invoked DisplayJobDetails method in a new Job instance

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -5,14 +5,25 @@ class Program
     static void Main(string[] args)
     {
         //Console.WriteLine("Hello Learning02 World!");
-    // Job instance
+    // Job instances
     Job job1 = new Job();
     job1._company = "Microsoft";
     job1._jobTitle = "C.E.O";
     job1._startYear = 1900;
     job1._endYear = 2000;
 
+    Job job2 = new Job();
+    job2._company = "Apple";
+    job2._jobTitle = "C.E.O";
+    job2._startYear = 2000;
+    job2._endYear = 2023;
+
     Console.WriteLine("-------------------------------------");
     job1.Display();
+    Console.WriteLine("-------------------------------------");
+    job1.DisplayJobDetails();
+    Console.WriteLine("-------------------------------------");
+    job2.DisplayJobDetails();
+    Console.WriteLine("-------------------------------------");
     }
 }
