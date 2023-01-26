595e86ab778bbcbaa23920ee4af6c10e7ea5bd0b
Robert Hypax Ndiramiye <ndiramiye.robert@gmail.com>
2023-01-26 14:22:12 +0200

    Invoked the display method in Job instance

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        //Console.WriteLine("Hello Learning02 World!");
+    // Job instance
+    Job job1 = new Job();
+    job1._company = "Microsoft";
+    job1._jobTitle = "C.E.O";
+    job1._startYear = 1900;
+    job1._endYear = 2000;
+
+    Console.WriteLine("-------------------------------------");
+    job1.Display();
     }
 }
