1ed5387615293cff7605a34af36e98af15182f76
Robert Hypax Ndiramiye <ndiramiye.robert@gmail.com>
2023-01-27 19:55:45 +0200

    Fixed menu loop

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -8,29 +8,25 @@ class Program
         Console.WriteLine("---------------------------");
         Console.WriteLine("Welcome to Your journal");
         Console.WriteLine("----------------------------------------------");
+        //Menu
+        int newNumber = 0;
+        while (newNumber != 5)
+        {
+            //Asking user for input
+            List<int> numbers = new List<int>();
+            Console.WriteLine("Please select one of the following options:");
+            
+            Console.WriteLine("1. Write");
+            Console.WriteLine("2. Display");
+            Console.WriteLine("3. Load");
+            Console.WriteLine("4. Save");
+            Console.WriteLine("5. Quit");
+            Console.WriteLine("----------------------------------------------");
+            Console.Write("What would you like to do? ");
+            string newNum = Console.ReadLine();
+            newNumber = int.Parse(newNum);
 
-        //Asking user for input
-        List<int> numbers = new List<int>();
-        Console.WriteLine("Please select one of the following options:");
-        
-        Console.WriteLine("1. Write");
-        Console.WriteLine("2. Display");
-        Console.WriteLine("3. Load");
-        Console.WriteLine("4. Save");
-        Console.WriteLine("5. Quit");
-        Console.WriteLine("----------------------------------------------");
-        Console.Write("What would you like to do? ");
-        string newNum = Console.ReadLine();
-        int newNumber = int.Parse(newNum);
-
-        //put the above in a loop that adds to the list until someone hits zero
-       while (newNumber != 5)
-       {
-            Console.WriteLine("Wazzaaa!");
-            string chosenNumber = Console.ReadLine();
-            Console.WriteLine(chosenNumber);
-            newNumber = int.Parse(chosenNumber);
-       }
+        }
 
     }
 }
