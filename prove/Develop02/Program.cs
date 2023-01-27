d48f9304e3e275e7cb77fe0bc93dfaebe82231aa
Robert Hypax Ndiramiye <ndiramiye.robert@gmail.com>
2023-01-27 08:35:51 +0200

    Created and tested menu

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop02 World!");
+        //Welcome message
+        Console.WriteLine("---------------------------");
+        Console.WriteLine("Welcome to Your journal");
+        Console.WriteLine("----------------------------------------------");
+
+        //Asking user for input
+        List<int> numbers = new List<int>();
+        Console.WriteLine("Please select one of the following options:");
+        
+        Console.WriteLine("1. Write");
+        Console.WriteLine("2. Display");
+        Console.WriteLine("3. Load");
+        Console.WriteLine("4. Save");
+        Console.WriteLine("5. Quit");
+        Console.WriteLine("----------------------------------------------");
+        Console.Write("What would you like to do? ");
+        string newNum = Console.ReadLine();
+        int newNumber = int.Parse(newNum);
+
+        //put the above in a loop that adds to the list until someone hits zero
+       if (newNumber != 5)
+       {
+            Console.WriteLine("Wazzaaa!");
+       }
+
     }
 }
