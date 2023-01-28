c6e59b1693ab9d48b41d528c3c9b34f64472662e
Robert Hypax Ndiramiye <ndiramiye.robert@gmail.com>
2023-01-28 08:43:32 +0200

    Created PromptGenerator class

---
+++ prove/Develop02/PromptGenerator.cs
@@ -0,0 +1,23 @@
+using System;
+
+public class PromptGenerator
+{
+    List<string> _encouragement = new List<string>()
+    {
+        "Your daily actions create your personality. Are you becoming someone all parts of your mind agree with?",
+        "Would you describe small actions you can do right now that would can correct any mistake you have done in the past?",
+        "What is something you did that made you feel competent today?",
+        "Is there a hard problem you can break in small parts now, so that the solution might become clearer?",
+        "What have you learned from the things that went wrong in your life?",
+        "What have you learned from the things that went right in your life?",
+        "Look around you, put something that isn't in order back in order and write down how it makes you feel?",
+        "Without being unfair to others, How can you defend and assert yourself today?",
+        "What are the first three steps to begin doing something helpful for you and others that yo can do today?",
+        "What can you do to encourage the people you love to keep becoming better humans?"
+    };
+    
+    public string ReturnPrompt(int number)
+    {
+        return _encouragement[number];
+    }
+}
