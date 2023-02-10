9d0224b772141ee998e8f8c8407c007119ef6da9
Robert Hypax Ndiramiye <ndiramiye.robert@gmail.com>
2023-02-10 23:55:14 +0200

    Used gettes and setters of the Fraction class

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,22 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction oneFraction = new Fraction();
+        oneFraction.getFractionString();
+        oneFraction.getDecimalValue();
+
+        Fraction secondFraction = new Fraction(5);
+        secondFraction.getFractionString();
+        secondFraction.getDecimalValue();
+
+        Fraction thirdFraction = new Fraction(3, 4);
+        thirdFraction.getFractionString();
+        thirdFraction.getDecimalValue();
+
+        Fraction lastFraction = new Fraction(1, 3);
+        lastFraction.getFractionString();
+        lastFraction.getDecimalValue();
+        Console.WriteLine("----------------------------");     
+        
     }
 }
