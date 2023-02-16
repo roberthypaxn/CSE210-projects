319f30e0944032ee0d8ddd06fad5b7a91172caf8
Robert Hypax Ndiramiye <ndiramiye.robert@gmail.com>
2023-02-16 16:42:57 +0200

    Updated the functionality of all main program: I will survive CSE 210!

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -4,6 +4,43 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop03 World!");
+        Scripture scripture = new Scripture();
+        scripture.Erase();
+        Console.WriteLine("");
+        string firstTime = scripture.displayWhole();
+        
+        Console.Write("\nPress Enter to continue or type 'quit' to finish: ");
+        string doThisNext = Console.ReadLine();
+        string hidden = scripture.Hidden(firstTime);
+        while (doThisNext.ToUpper() != "QUIT")
+        {
+            scripture.Erase();
+            Console.WriteLine(hidden);
+
+            // check if all non-space, non-first two words are hidden
+            bool allHidden = true;
+            string[] hiddenWords = hidden.Split(' ');
+            for (int i = 2; i < hiddenWords.Length; i++)
+            {
+                if (!hiddenWords[i].All(c => c == '-'))
+                {
+                    allHidden = false;
+                    break;
+                }
+            }
+
+            if (allHidden)
+            {
+                doThisNext = "QUIT";
+            }
+            else
+            {
+                hidden = scripture.Hidden(hidden);
+                Console.Write("\nPress Enter to continue or type 'quit' to finish: ");
+                doThisNext = Console.ReadLine();
+            }
+        }
+
+
     }
 }
