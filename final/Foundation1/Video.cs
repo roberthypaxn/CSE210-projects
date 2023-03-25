ee55c2af0f8431df04ffcae8dc82dda92f77acc0
Robert Hypax Ndiramiye <ndiramiye.robert@gmail.com>
2023-03-25 12:01:08 +0200

    populated video class

---
+++ final/Foundation1/Video.cs
@@ -0,0 +1,12 @@
+using System;
+
+class Video
+{
+    private string _title;
+    private string _author;
+    private  int _length;
+    private List<List<string>> _comments;
+    public Video()
+    {
+    }
+}
