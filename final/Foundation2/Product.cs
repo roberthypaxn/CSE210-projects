4ba1c7b360ba0661be3ff4a721fdae862045cc39
Robert Hypax Ndiramiye <ndiramiye.robert@gmail.com>
2023-03-28 13:54:32 +0200

    Created product class

---
+++ final/Foundation2/Product.cs
@@ -0,0 +1,16 @@
+using System;
+
+class Product
+{
+    internal object _productId;
+    internal string _productName;
+    internal int _price;
+    private int _quantity;
+    public Product(string name, string id, int price, int quantity)
+    {
+        _productName = name;
+        _productId = id;
+        _price = price * quantity;
+        _quantity = quantity;
+    }
+}
